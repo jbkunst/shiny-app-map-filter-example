94a5291a43cc186ea26215b1c7dfae57ab466ff9
Joshua Kunst <jbkunst@gmail.com>
2023-11-13 13:05:54 -0300

    Update global.R

--- global.R
+++ global.R
@@ -1,16 +1,19 @@
 # packages ----------------------------------------------------------------
 library(shiny)
 library(bslib)
-library(leaflet)
 library(tidyverse)
 library(bsicons)
+library(leaflet)
+library(highcharter)
 
 # data --------------------------------------------------------------------
 # opcion 1
 # data <- read_csv("https://raw.githubusercontent.com/Appsilon/crossfilter-demo/master/app/ships.csv")
 
 # opcion 2
-data <- as_tibble(quakes)
+data <- as_tibble(quakes) |> 
+  mutate(mag_cat = ggplot2::cut_interval(mag, 3))
 
+levels(data$mag_cat) <-  c("Bajo", "Medio", "Alto")
 
 
